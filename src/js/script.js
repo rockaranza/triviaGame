b1ddcba05414084f34fbc14eb24ed8d20e0f99c0
rockaranza <diego.ocaranza@outlook.com>
2021-08-21 23:37:15 -0400

    Integrando JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,28 @@
 const sumarPuntos = document.getElementById("btnSumar");
+const restarVida = document.getElementById("btnPerder");
+const corazon1 = document.getElementById("heart1");
+const corazon2 = document.getElementById("heart2");
+const corazon3 = document.getElementById("heart3");
 
+const score = document.getElementById("score");
+
+let corazones = 0;
+
+/*Calcular puntaje*/
+function contadorPuntaje (){
+    let calculoPuntaje = score.innerText;
+    console.log(score.innerText);
+    calculoPuntaje = parseInt(calculoPuntaje) + 100;
+    console.log(calculoPuntaje);
+    score.innerText = "";
+    score.innerText = `${calculoPuntaje}`;
+    console.log("funcions");
+}
+
+/*Perder Vida*/
+function perderVida(corazones) {
+    corazon3.classList.toggle("is-transparent");
+}
+
+sumarPuntos.addEventListener("click", contadorPuntaje);
+restarVida.addEventListener("click", perderVida);
