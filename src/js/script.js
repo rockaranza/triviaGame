32f494287658c41403c4e06604035f165f850a5a
Diego Ocaranza <diego.ocaranza@outlook.com>
2021-08-22 14:17:59 -0400

    Quitar vidas, sumar puntos y barra de progreso funcionando

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,18 @@
 const sumarPuntos = document.getElementById("btnSumar");
 const restarVida = document.getElementById("btnPerder");
+const progresar = document.getElementById("btnProgreso");
+
+/*Contenido HTML*/
 const corazon1 = document.getElementById("heart1");
 const corazon2 = document.getElementById("heart2");
 const corazon3 = document.getElementById("heart3");
-
+const barraProgreso = document.getElementById("barraProgreso");
 const score = document.getElementById("score");
 
-let corazones = 0;
 
 /*Calcular puntaje*/
 function contadorPuntaje (){
-    let calculoPuntaje = score.innerText;
-    console.log(score.innerText);
+    let calculoPuntaje = parseInt(score.innerHTML);
     calculoPuntaje = parseInt(calculoPuntaje) + 100;
     console.log(calculoPuntaje);
     score.innerText = "";
@@ -19,10 +20,25 @@ function contadorPuntaje (){
     console.log("funcions");
 }
 
+/*Aumentar linea de progreso*/
+function progresarBarra(){
+    let progresoActual = parseInt(barraProgreso.getAttribute("value"));
+    progresoActual = progresoActual + 10;
+    barraProgreso.setAttribute("value", progresoActual);
+    console.log(progresoActual);
+}
+
 /*Perder Vida*/
-function perderVida(corazones) {
-    corazon3.classList.toggle("is-transparent");
+function perderVida() {
+    if(!corazon3.classList.contains("is-transparent")){
+        corazon3.classList.add("is-transparent");
+    }else if(!corazon2.classList.contains("is-transparent")){
+        corazon2.classList.add("is-transparent");
+    }else if(!corazon1.classList.contains("is-transparent")){
+        corazon1.classList.add("is-transparent");
+    }
 }
 
 sumarPuntos.addEventListener("click", contadorPuntaje);
 restarVida.addEventListener("click", perderVida);
+progresar.addEventListener("click", progresarBarra);
